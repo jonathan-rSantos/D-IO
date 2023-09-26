4898e896449fe8b42c1a8decc8c4e455c01aed6c
Jonathan <jonathan.ribeiro28121997@gmail.com>
2023-09-26 00:01:34 -0300

    controle de fluxo de candidatos a vaga de trabalho

---
+++ src/candidatura/ProcessoSeletivo.java
@@ -0,0 +1,25 @@
+package candidatura;
+
+import java.util.Scanner;
+
+public class ProcessoSeletivo {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Processo seletivo");
+		double pretencao =	sc.nextDouble();
+		Analisarcandidato(pretencao);
+	}
+
+	static void Analisarcandidato(double salarioPretendido) {
+		double salarioBase = 2000.0;
+		if(salarioBase > salarioPretendido) {
+			System.out.println("Ligar para o candidato");
+		} else if(salarioBase == salarioPretendido){
+				System.out.println("Ligar para o candidato, com contra proposta");
+		} else {
+			System.out.println("Aguardando resultado dos demais candidatos");
+		}
+	}
+}
